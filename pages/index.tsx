989da33b2e070126ba0f5d5c94fb3694c0eb6681
Hyunsoo Lee <wrkbrs@gmail.com>
2022-06-27 23:03:08 +0900

    update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
+import Widgets from "../components/Widgets";
 
 const Home: NextPage = () => {
   return (
@@ -10,12 +11,12 @@ const Home: NextPage = () => {
         <title>Twitter 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className="grid grid-cols-9">
         <Sidebar />
 
         <Feed />
 
-        {/* Widgets */}
+        <Widgets />
       </main>
     </div>
   );
