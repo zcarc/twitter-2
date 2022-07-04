295e24f340fbecf4c4659df1580c6c817e220623
Hyunsoo Lee <wrkbrs@gmail.com>
2022-07-04 22:45:41 +0900

    config sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
 import Widgets from "../components/Widgets";
+import { fetchTweets } from "../utils/fetchTweets";
 
 const Home: NextPage = () => {
   return (
@@ -23,3 +24,11 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const tweets = await fetchTweets();
+
+  return {
+    props: {},
+  };
+};
