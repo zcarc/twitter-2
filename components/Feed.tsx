223c9c8423e38dec94b2b24ba30892e7754b5b0a
Hyunsoo Lee <wrkbrs@gmail.com>
2022-07-15 21:01:06 +0900

    implement feed refresh icon

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,6 +1,7 @@
 import { RefreshIcon } from "@heroicons/react/outline";
-import React from "react";
+import React, { useState } from "react";
 import { Tweet } from "../typings";
+import { fetchTweets } from "../utils/fetchTweets";
 import TweetComponent from "./Tweet";
 import TweetBox from "./TweetBox";
 
@@ -8,12 +9,20 @@ interface Props {
   tweets: Tweet[];
 }
 
-function Feed({ tweets }: Props) {
+function Feed({ tweets: tweetsProps }: Props) {
+  const [tweets, setTweets] = useState<Tweet[]>(tweetsProps);
+  console.log("Feed... tweets: ", tweets);
+
+  const handleRefresh = async () => {
+    const tweets = await fetchTweets();
+    setTweets(tweets);
+  };
   return (
     <div className="col-span-7 lg:col-span-5 border-x">
       <div className="flex items-center justify-between">
         <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
         <RefreshIcon
+          onClick={handleRefresh}
           className="mr-5 mt-5 h-8 w-8 cursor-pointer
           text-twitter transition-all duration-500 ease-out
           hover:rotate-180 active:scale-125"
