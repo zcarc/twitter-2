40e472938c725c80c9813d9d13e932530ceef1ec
Hyunsoo Lee <wrkbrs@gmail.com>
2022-07-15 21:00:31 +0900

    implement comment functionality

--- typings.d.ts
+++ typings.d.ts
@@ -1,3 +1,10 @@
+export interface TweetBody {
+  text: string;
+  username: string;
+  profileImg: string;
+  image?: string;
+}
+
 export interface Tweet extends TweetBody {
   _id: string;
   _createdAt: string;
@@ -7,9 +14,21 @@ export interface Tweet extends TweetBody {
   blockTweet: boolean;
 }
 
-export interface TweetBody {
-  text: string;
+export interface CommentBody {
+  comment: string;
+  tweetId: string;
   username: string;
   profileImg: string;
-  image?: string;
+}
+
+export interface Comment extends CommentBody {
+  _id: string;
+  _createdAt: string;
+  _updatedAt: string;
+  _rev: string;
+  _type: "comment";
+  tweet: {
+    _ref: string;
+    _type: "reference";
+  };
 }
